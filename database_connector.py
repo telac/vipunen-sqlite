d56d1ee8d0b1e3a77deaddf9fc713e69616621dc
Christian Westerlund <christian.westerlund@tek.fi>
2018-04-16 15:28:35 +0300

    initial commit, working base solution

---
+++ database_connector.py
@@ -0,0 +1,53 @@
+import sqlite3
+
+class DatabaseConnector(object):
+
+    def __init__(self, name, data, meta):
+        self.name = name
+        self.data = data
+        self.meta = meta
+        self.variables = []
+        self.database = 'vipunen.db'
+        self.connection = sqlite3.connect(self.database)
+        self.cursor = self.connection.cursor()
+
+    def create_schema(self):
+        # apparently parameter substitution is not available for creating tables
+        # so this part might and probably is slightly unsafe. It's all created in a static database though.
+        sql_cmd = ("""CREATE TABLE IF NOT EXISTS {} (uid INTEGER PRIMARY KEY, """.format(self.name))
+        self.variables.append('?')
+        attributes = []
+        # well this is pretty dumb.
+        replace_list = [(':', '_'), ('-', '_'), (',','_'), ('(', '_'), (')', '_')]
+        for column in self.meta:
+            name = column['name']
+            for r in replace_list:
+                name = name.replace(*r)
+            type_ = column['type']
+            if type_ == 'number':
+                type_ = 'INTEGER'
+            else:
+                type_ = 'TEXT'
+            attributes.append(name + ' ' + type_)
+            self.variables.append('?')
+
+        attributes_ = ', '.join(attributes)
+        sql_cmd += """{});""".format(attributes_)
+        print(sql_cmd)
+        self.cursor.execute(sql_cmd)
+        self.connection.commit()
+
+    def insert_data(self):
+        variables = ','.join(self.variables)
+        # result could look like e.g.
+        # 'INSERT INTO avoin_yliopisto VALUES (?, ?, ?, ?, ?, ?)'
+        insert_cmd = 'INSERT INTO {} VALUES ({});'.format(self.name, variables)
+        print(insert_cmd)
+        values = []
+        for index, row in enumerate(self.data):
+            values.append(index)
+            for key in row:
+                values.append(row[key])
+            self.cursor.execute(insert_cmd, values)
+            values = []
+        self.connection.commit()
---
+++ load_data.py
@@ -0,0 +1,39 @@
+import requests
+import database_connector
+import json
+
+class APIConnector(object):
+
+    def __init__(self):
+        self.resources_url = 'http://api.vipunen.fi/api/resources'
+
+    def get_metadata(self, content):
+        metadata = requests.get(self.resources_url + '/' + content)
+        return metadata.json()
+
+    def get_data(self, content):
+        data = requests.get(self.resources_url + '/' + content + '/data')
+        return data.json()
+
+    def get_resources(self):
+        resources = requests.get(self.resources_url)
+        return resources.json()
+
+    def start_parse(self):
+        for x in self.get_resources():
+            try:
+                meta = self.get_metadata(x)
+                data = self.get_data(x)
+                connector = database_connector.DatabaseConnector(x, data, meta)
+                connector.create_schema()
+                connector.insert_data()
+
+            except json.JSONDecodeError:
+                print("corrupt json: " + x + " \nmoving on to next file")
+                pass
+
+
+if __name__ == '__main__':
+    AC = APIConnector()
+    AC.start_parse()
+
